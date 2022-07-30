d8f2fee64d3f171a87a0356aac7ae2050c642fcf
Vicks <vk82562425@gmail.com>
2022-07-31 03:14:51 +0530

    lecture 70 completed

--- routes/auth.js
+++ routes/auth.js
@@ -14,24 +14,25 @@ router.post('/createuser', [
     body('email', 'Enter valid email').isEmail(),
     body('password', 'Password should be more than 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+    let success = false;
     //if there are error, return bad request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array(), mess: "hiii" });
     }
+
     //check whether email already exist
     try{
     let user = await User.findOne({email: req.body.email});
 
     if(user){
-        return res.status(400).json({error: "This email is already registered"});
+        return res.status(400).json({success, error: "This email is already registered"});
     }
     //create new user
-    const salt = await bcrypt.genSaltSync(10);
-    const secretPass = await bcrypt.hashSync(req.body.password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    const secretPass = bcrypt.hashSync(req.body.password, salt);
     user = await User.create({
         name: req.body.name,
-        username: req.body.username,
         email: req.body.email,
         password: secretPass,
       })
@@ -43,10 +44,11 @@ router.post('/createuser', [
         const authToken = jwt.sign(data, JWT_SECRET);
         
         //res.json(user)
-        res.json({authToken});
+        success = true;
+        res.json({success, authToken});
     }
     catch(err){
-        res.json({message: err.message});
+        res.json({success, message: err.message, mess: "hello"});
         res.status(500).send("Internal server error occured");
     }
 })
@@ -56,21 +58,22 @@ router.post('/login', [
     body('email', 'Enter valid email').isEmail(),
     body('password', 'Enter valid password').isLength({ min: 5 }),
 ], async (req, res) => {
+    let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array() });
     }
 
     const {email, password} = req.body;
     try {
         let user = await User.findOne({email});
         if(!user){
-            return res.status(400).json({error: "Login with correct credentials"});
+            return res.status(400).json({success, error: "Login with correct credentials"});
         }
 
         const isValidPass = await bcrypt.compare(password, user.password);
         if(!isValidPass){
-            return res.status(400).json({error: "Login with correct credentials"});
+            return res.status(400).json({success, error: "Login with correct credentials"});
         }
 
         const data = {
@@ -79,10 +82,11 @@ router.post('/login', [
             }
         }
         const authToken = jwt.sign(data, JWT_SECRET);
-        res.json({authToken});
+        success = true;
+        res.json({success, authToken});
 
     } catch (error) {
-        res.json({message: error.message});
+        res.json({success, message: error.message});
         res.status(500).send("Internal server error occured");
     }
 })
