72db489352fd5673c729e58d5c7146b5cb679c3e
Vicks <vk82562425@gmail.com>
2022-07-31 18:24:29 +0530

    message

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://vicks-825:pass1234pass@e-notebook.w7ro1.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://vicks-825:pass1234pass@e-notebook.w7ro1.mongodb.net/e-notebook?retryWrites=true&w=majority";
 
 const connetToMongo = () => {
     mongoose.connect(mongoURI, () => {
