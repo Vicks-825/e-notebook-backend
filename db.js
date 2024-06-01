13ac721d305b60795ee1272cb007c88d77dfe2d4
Vicky Kumar <vicky.kumar@bounteous.com>
2024-06-01 17:59:22 +0530

    Fixed the errors

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://vicks-825:pass1234pass@e-notebook.w7ro1.mongodb.net/e-notebook?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://vk82562425:KuUlXrAymE9h92Qd@e-notebook-cluster.ohqjkog.mongodb.net/?retryWrites=true&w=majority&appName=e-notebook-cluster";
 
 const connetToMongo = () => {
     mongoose.connect(mongoURI, () => {
--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const cors = require('cors');
 connetToMongo();
 
 const app = express()
-const port = 5000
+const port = 8000
 
 app.use(cors())
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Vicky you are really great!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello Vicky you are really great!')
+})
 
 //Available routes
 app.use('/api/auth/', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log(`E-Notebook app connected`);
 })
